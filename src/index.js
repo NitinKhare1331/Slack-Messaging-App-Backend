9fc9f35ee6ee78ce83f71ab302e2e127dbf4c3ee
Nitin Khare <kharenitin777@gmail.com>
2024-11-09 11:47:16 +0000

    Added DB Config for mongo

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import express from 'express'
 import { StatusCodes } from 'http-status-codes'
+
+import connectDB from './config/dbConfig.js'
 import { PORT } from './config/serverConfig.js'
 
 const app = express()
 
-app.get('/ping', (Req, res) => {
-  return res.status(StatusCodes.OK).json({ message: 'pong' })
+app.get('/ping', (req, res) => {
+    return res.status(StatusCodes.OK).json({ message: 'pong' })
 })
 
 app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`)
+    console.log(`Server is running on ${PORT}`);
+    connectDB();
 })
+
