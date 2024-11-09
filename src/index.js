e93e8100433daa4b1d2449f491db3d8287a6e625
Nitin Khare <kharenitin777@gmail.com>
2024-11-09 12:01:07 +0000

    Added basic routing layer

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import { StatusCodes } from 'http-status-codes'
 
 import connectDB from './config/dbConfig.js'
 import { PORT } from './config/serverConfig.js'
+import apiRouter from './routes/apiRouter.js'
 
-const app = express()
+const app = express();
+
+app.use('/api', apiRouter);
 
 app.get('/ping', (req, res) => {
     return res.status(StatusCodes.OK).json({ message: 'pong' })
