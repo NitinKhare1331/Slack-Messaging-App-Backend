b5f08e358507a5b059f35f1b5c73517c46947460
Nitin Khare <kharenitin777@gmail.com>
2024-11-17 12:15:33 +0530

    added isAuth middleware

--- src/routes/v1/users.js
+++ src/routes/v1/users.js
@@ -3,10 +3,11 @@ import express from 'express';
 import { signIn, signUp } from '../../controllers/userController.js';
 import { userSignInSchema, userSignUpSchema } from '../../validators/userSchema.js';
 import { validate } from '../../validators/zodValidator.js';
+import { isAuthenticated } from '../../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/signup', validate(userSignUpSchema), signUp);
-router.post('/signin', validate(userSignInSchema), signIn);
+router.post('/signin',isAuthenticated, validate(userSignInSchema), signIn);
 
 export default router;
