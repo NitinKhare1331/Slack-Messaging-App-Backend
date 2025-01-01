fcdfbcd76cf0ca5a69d640fc29c4b7ae79802419
Nitin Khare <kharenitin777@gmail.com>
2025-01-01 17:47:39 +0530

    Bug fix

--- src/repositories/messageRepository.js
+++ src/repositories/messageRepository.js
@@ -6,7 +6,7 @@ export const messageRepository = {
 
     getPaginatedMessages: async (messageParams, page, limit) => {
         const message = await Message.find(messageParams)
-            .sort({ createdAt: 1 })
+            .sort({ createdAt: -1 })
             .skip(( page - 1)*limit)
             .limit(limit)
             .populate('senderId', 'username email avatar');
