365a3203a218559b03ff3d12e23dca913f94acdf
Nitin Khare <kharenitin777@gmail.com>
2025-01-06 19:48:21 +0530

    Added get object url api

--- src/routes/v1/message.js
+++ src/routes/v1/message.js
@@ -1,10 +1,13 @@
 import express from "express";
 
-import { getMessagesController } from "../../controllers/messageController.js";
+import { getMessagesController, getPresignedUrlFromAWS } from "../../controllers/messageController.js";
 import { isAuthenticated } from '../../middlewares/authMiddleware.js'
 
 const router = express.Router();
 
+router.get('/pre-signed-url', isAuthenticated, getPresignedUrlFromAWS);
+
 router.get('/:channelId', isAuthenticated, getMessagesController);
+
 
 export default router;
